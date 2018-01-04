1e7c087a1e5f78b43a76265cb531e5ee82a57217
Markus Jaeger <jaeger.markus@gmx.at>
2018-01-04 20:00:37 +0100

    Showing/hiding content list + updated marker style

--- base.js
+++ base.js
@@ -10,11 +10,20 @@ function initMap() {
     zoom: 11
   });
 
+  peaksViewModel.test();
+
+  // Create a "highlighted location" marker color
+  var highlightMarker = makeMarkerIcon('AD1457');
+
+  // Default marker
+  var defaultMarker = makeMarkerIcon('00BCD4');
+
   peaksArray.forEach(function(peak){
     var marker = new google.maps.Marker({
       position: {lat: peak.lat, lng: peak.lng},
       title: peak.name,
-      animation: google.maps.Animation.DROP
+      animation: google.maps.Animation.DROP,
+      icon: defaultMarker
     });
     markers.push(marker);
   });
@@ -33,4 +42,23 @@ function initMap() {
   }
 
 
+  // This function takes in a COLOR, and then creates a new marker
+  // icon of that color. The icon will be 21 px wide by 34 high, have an origin
+  // of 0, 0 and be anchored at 10, 34).
+  // Source: /ud864/Project_Code_5_BeingStylish.html
+  function makeMarkerIcon(markerColor) {
+    var markerImage = new google.maps.MarkerImage(
+      'http://chart.googleapis.com/chart?chst=d_map_spin&chld=1.15|0|'+ markerColor +
+      '|40|_|%E2%80%A2',
+      new google.maps.Size(21, 34),
+      new google.maps.Point(0, 0),
+      new google.maps.Point(10, 34),
+      new google.maps.Size(21,34));
+    return markerImage;
+    }
+
+
+  // function highlightPeak(i) {
+  //   console.log(i);
+  // }
 }
--- viewmodel.js
+++ viewmodel.js
@@ -1,13 +1,42 @@
 
-// Activates knockout.js
-
-
 var ViewModel = function() {
 
   // Drop initial markers
   peaksArray.forEach(function(peak){
-    console.log(peak.name);
+    // console.log(peak.name);
   });
+
+  // Handles showing/hiding of content box
+
+  this.showBox = ko.observable(true); // Box initially visible
+
+  // Function that toggles  visible
+  this.toggleBox = function() {
+      if (this.showBox() === true){
+        this.showBox(false);
+      }
+      else {
+        this.showBox(true);
+      }
+  };
+
+  // Function handling clicks on peaks in list
+  this.clickedPeak = function(peak) {
+    // console.log(peak);
+    // console.log(peaksArray.indexOf(peak));
+    highlightPeak(peaksArray.indexOf(peak));
+  };
+
+
+  this.test = function() {
+    console.log("test");
+  };
+
 }
 
-ko.applyBindings(new ViewModel());
+// Initialising ViewModel
+var peaksViewModel = new ViewModel();
+ko.applyBindings(peaksViewModel);
+
+
+// All map related actions
